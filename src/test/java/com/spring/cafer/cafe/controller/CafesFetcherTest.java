eefde83dfbbd1c3d56a3f7bd2237368b4143aa73
jackson-hong <jacksonhong0111@gmail.com>
2024-07-07 22:51:16 +0900

    test : graphql query builder를 통한 fetcher test

--- src/test/java/com/spring/cafer/cafe/controller/CafesFetcherTest.java
+++ src/test/java/com/spring/cafer/cafe/controller/CafesFetcherTest.java
@@ -3,6 +3,9 @@ package com.spring.cafer.cafe.controller;
 import com.netflix.graphql.dgs.DgsQueryExecutor;
 import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration;
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
+import com.spring.cafer.client.CafeGraphQLQuery;
+import com.spring.cafer.client.CafeProjectionRoot;
+import com.spring.cafer.client.CafesGraphQLQuery;
 import com.spring.cafer.domain.cafe.controller.CafesFetcher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +23,18 @@ public class CafesFetcherTest {
 
     @Test
     void testCafesQuery() {
-        List<String> cafes = dgsQueryExecutor.executeAndExtractJsonPath(
-                " { cafes { id name description location } }",
-                "data.cafes[*].name");
+//        직접 쿼리를 입력하는 방식
+//        List<String> cafes = dgsQueryExecutor.executeAndExtractJsonPath(
+//                " { cafes { id name description location } }",
+//                "data.cafes[*].name");
 
+//      쿼리 빌더 방식
         GraphQLQueryRequest graphQLQueryRequest = new GraphQLQueryRequest(
-        )
+                CafesGraphQLQuery.newRequest().build(),
+                new CafeProjectionRoot<>().name()
+        );
+
+        List<String> cafes = dgsQueryExecutor.executeAndExtractJsonPath(graphQLQueryRequest.serialize(),"data.cafes[*].name");
 
         assertThat(cafes).contains("jackson");
     }
